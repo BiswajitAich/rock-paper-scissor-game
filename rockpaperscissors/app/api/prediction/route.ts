da203b4f8ca3982d1bcc81265fd89554e0a3d938
Biswajit Aich <0000biswajitaich@gmail.com>
2024-11-27 17:23:45 +0530

    add: api

--- rockpaperscissors/app/api/prediction/route.ts
+++ rockpaperscissors/app/api/prediction/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
+const API: string | undefined = process.env.ROCK_PAPER_SCISSOR_FLASK_API;
+if (!API) {
+    throw new Error("ROCK_PAPER_SCISSOR_FLASK_API is not defined in environment variables.");
+}
 export async function POST(request: NextRequest) {
     try {
         const formData = await request.formData();
@@ -6,15 +10,25 @@ export async function POST(request: NextRequest) {
         if (!file) {
             return new Response(JSON.stringify({ error: 'No file uploaded' }), { status: 400 });
         }
-        const response = await fetch("http://localhost:5000/predict", {
+        const response = await fetch(API!, {
             method: "POST",
             body: formData,
+            headers: {
+                "Accept": "application/json"
+            },
         });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.error("Flask API Error:", errorData);
+            return NextResponse.json({ error: 'Failed to process image' }, { status: response.status });
+        }
 
         const data = await response.json();
         console.log("API Response:", data);
         return new Response(JSON.stringify({ 'data': data.result.toLowerCase() }), { status: 200 })
     } catch (error) {
         console.error("Error sending to Flask:", error);
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
     }
 }
