510065f1864cbfef67a02854dcc77531e9c96924
Biswajit Aich <0000biswajitaich@gmail.com>
2024-11-28 00:24:29 +0530

    try 2

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from PIL import Image
 import os
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 MODEL_PATH = "model/rock-paper-scissors-best.pt"
 model = YOLO(MODEL_PATH)
 @app.route('/predict', methods=['POST'])
