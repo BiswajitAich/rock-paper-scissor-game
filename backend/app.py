414a2bf2441f4b95e5ea5c3225f6f09956891eb8
Biswajit Aich <0000biswajitaich@gmail.com>
2024-11-27 17:04:47 +0530

    added: gunicorn

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 from flask import Flask, request, jsonify
 from PIL import Image
+import os
 app = Flask(__name__)
 MODEL_PATH = "model/rock-paper-scissors-best.pt"
 model = YOLO(MODEL_PATH)
@@ -26,4 +27,5 @@ def health_check():
     return jsonify({'status': 'OK'}), 200
     
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
